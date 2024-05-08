a1ac3c2050d142f665507a8add56b7cfc0117a6e
Kevin Laughlin <kevinalaughlin@gmail.com>
2024-05-08 16:21:26 -0700

    Improved class naming conventions. Added functionality to hero section.

--- main.js
+++ main.js
@@ -1,25 +1,66 @@
+let pokemonName = "";
+let pokemonId = 0;
+let pokemonType = [];
+let pokemonImg = "";
+let pokemonHp = 0;
+let pokemonAtk = 0;
+let pokemonDef = 0;
+let pokemonSpAtk = 0;
+let pokemonSpDef = 0;
+let pokemonSpd = 0;
+let pokemonPrimaryType = "";
+let pokemonSecondaryType = "";
+
 async function getPokemonStats(num) {
     let url = "https://pokeapi.co/api/v2/pokemon/" + num.toString();
 
     let res = await fetch(url);
     let pokemon = await res.json();
-    // console.log(pokemon);
 
-    let pokemonName = pokemon["name"];
-    let pokemonType = pokemon["types"];
-    let pokemonImg = pokemon["sprites"]["front_default"];
-    let pokemonHp = pokemon["stats"]["0"]["base_stat"];
-    let pokemonAtk = pokemon["stats"]["1"]["base_stat"];
-    let pokemonDef = pokemon["stats"]["2"]["base_stat"];
-    let pokemonSpAtk = pokemon["stats"]["3"]["base_stat"];
-    let pokemonSpDef = pokemon["stats"]["4"]["base_stat"];
-    let pokemonSpd = pokemon["stats"]["5"]["base_stat"];
+    pokemonName = pokemon["name"];
+    pokemonId = pokemon["id"];
+    pokemonType = pokemon["types"];
+    pokemonImg = pokemon["sprites"]["other"]["home"]["front_default"];
+    pokemonHp = pokemon["stats"][0]["base_stat"];
+    pokemonAtk = pokemon["stats"][1]["base_stat"];
+    pokemonDef = pokemon["stats"][2]["base_stat"];
+    pokemonSpAtk = pokemon["stats"][3]["base_stat"];
+    pokemonSpDef = pokemon["stats"][4]["base_stat"];
+    pokemonSpd = pokemon["stats"][5]["base_stat"];
+    pokemonPrimaryType = pokemonType[0]["type"]["name"];
 
-    console.log(pokemonName);
+    // Secondary type (check if it exists)
+    if (pokemonType.length > 1) {
+        pokemonSecondaryType = pokemonType[1]["type"]["name"];
+    } else {
+        pokemonSecondaryType = "None";
+    }
 }
 
 function getRandomPokemon() {
     return Math.floor(Math.random() * 1025) + 1;
 }
 
-getPokemonStats(getRandomPokemon());
+async function updateHeroPokemon() {
+    await getPokemonStats(getRandomPokemon());
+
+    // Update Pokémon image
+    const pokemonImage = document.getElementById("hero_pokemon");
+    pokemonImage.src = pokemonImg;
+    pokemonImage.alt = `Image of ${pokemonName}`;
+
+    // Update Pokémon name, order, and types
+    document.getElementById("hero_text--pokemon-name-number").innerHTML = `${pokemonName}\u00A0\u00A0\u00A0*\u00A0\u00A0\u00A0#${pokemonId}`;
+
+    const pokemonTypeText = document.getElementById("hero_text--type");
+    pokemonTypeText.innerHTML = `${pokemonPrimaryType}`;
+
+    if (pokemonSecondaryType !== "None") {
+        pokemonTypeText.innerHTML += `\u00A0\u00A0\u00A0+\u00A0\u00A0\u00A0${pokemonSecondaryType}`;
+    }
+}
+
+document.getElementById("testPokemon").addEventListener("click", async () => {
+    document.getElementById("hero_pokeball").classList.add("u-size-hidden"); // Shrinks Pokéball
+    await updateHeroPokemon();
+});
